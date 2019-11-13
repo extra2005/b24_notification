4a3c3d5d69b33a0d80af5da48131c530a6d3b0e3
Андрей Александрович Куренков <AAKurenkov@DVC.RU>
2019-11-13 14:16:56 +0300

    update meeting status

--- bitrix/php_interface/init.php
+++ bitrix/php_interface/init.php
@@ -10,12 +10,27 @@ class CalendarEditNotify{
 	static public function OnPageStartHandler(){
 			if($_REQUEST['markAction']=="editEvent" && $_REQUEST['id']>0)
 				self::$_currentEvent = CCalendarEvent::GetList(array('arFilter' => array("PARENT_ID" => (int)$_REQUEST['id'])));
+			return true;
 	}
 	
 	static public function OnCalendarEntryUpdateHandler($id,$entryFields){
 		$fromTo = CCalendarEvent::GetEventFromToForUser($entryFields, $entryFields['OWNER_ID']);
-		if(self::$_currentEvent['NAME']!=$entryFields['NAME'] || self::$_currentEvent['DATE_FROM']!=$fromTo['DATE_FROM'] || self::$_currentEvent['DATE_TO']!=$fromTo['DATE_TO']){
-			CCalendarNotify::ClearNotifications($entryFields['PARENT_ID']);
+		$event = CCalendarEvent::GetList(
+			array(
+				'arFilter' => array(
+					"ID" => $id,
+					"IS_MEETING" => 1,
+					"DELETED" => "N"
+				),
+				'parseRecursion' => false,
+				'fetchAttendees' => true,
+				'fetchMeetings' => true,
+				'checkPermissions' => false,
+				'setDefaultLimit' => false
+		));
+		
+		if($event[0]['MEETING_STATUS'] == "Q" && (self::$_currentEvent['NAME']!=$entryFields['NAME'] || self::$_currentEvent['DATE_FROM']!=$fromTo['DATE_FROM'] || self::$_currentEvent['DATE_TO']!=$fromTo['DATE_TO'])){
+			//CCalendarNotify::ClearNotifications($entryFields['PARENT_ID']);
 			CCalendarNotify::Send(array(
 				'mode' => 'invite',
 				'name' => $entryFields['NAME'],
@@ -23,8 +38,10 @@ class CalendarEditNotify{
 				'to' => $fromTo['DATE_TO'],
 				'location' => CCalendar::GetTextLocation($entryFields["LOCATION"]),
 				"guestId" => $entryFields['OWNER_ID'],
-				"eventId" => $id,
-				"userId" => $entryFields['~MEETING']['MEETING_CREATOR'],));
+				"eventId" => $entryFields['PARENT_ID'],//$id,
+				"userId" => $entryFields['~MEETING']['MEETING_CREATOR'],
+				"fields" => $event[0]));
 		}
+		return true;
 	}
 }
